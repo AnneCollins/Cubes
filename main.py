64e30aa9d3f8ecf741ef95a87b30c8d0dc1f18e5
Victor Shnayder <victor@mitx.mit.edu>
2013-01-13 15:18:41 -0500

    add skeleton shape class for Anne to play with

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 import json
 import sys
 
+class Shape(object):
+    def __init__(self, x):
+        print "Construct!"
+        self.x = x
+
+    def print_me(self):
+        print self.x
+
 def main(args):
     print "Welcome to the Cube Solver!!!"
     print args
+    s = Shape(args[0])
+    s.print_me()
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(sys.argv[1:])
